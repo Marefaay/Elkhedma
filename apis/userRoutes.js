3d699d6c3982f59a3ff843ed8b88006a6ed1e8e3
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-06-17 00:41:50 +0300

    Update userRoutes.js

--- apis/userRoutes.js
+++ apis/userRoutes.js
@@ -5,7 +5,7 @@ const loginAsUser = require("../services/user/loginAsUser");
 const profilePhoto = require("../services/user/profilePhoto");
 const registerAsUser = require("../services/user/registerAsUser");
 const scan = require("../services/Meeting/scan");
-const uploadQR = require("../middlewares/upload/QRSUpload");
+const uploadQR = require("../middlewares/upload/QRsUpload");
 // const uploadQR = require("../middlewares/upload/QRsUpload");
 const router = require("express").Router();
 
