9b4af9a585f2db355087dab5dab1d9648902d8fe
Marefaay <mrefaay271@gmail.com>
2024-09-09 19:44:49 +0300

    allUserEdit

--- services/user/allUsers.js
+++ services/user/allUsers.js
@@ -1,6 +1,9 @@
 const userModel = require("../../models/userModel");
 const allUsers = async (request, response) => {
-  const users = await userModel.find({});
+  const users = await userModel.find(
+    {},
+    { _id: 0, __v: 0, createdAt: 0, updatedAt: 0, isAdmin: 0 }
+  );
   const count = await userModel.find({}).count();
   if (users.length == 0) {
     return response.json({ status: "Error", message: "There Is No Student" });
