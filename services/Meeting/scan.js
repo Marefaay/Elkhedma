0c21f319b56332a935b08e89b8fa01adef28c11f
Mahmoud Refaay <125451007+Marefaay@users.noreply.github.com>
2024-10-17 14:05:00 +0300

    Update scan.js

--- services/Meeting/scan.js
+++ services/Meeting/scan.js
@@ -1,62 +1,72 @@
 const QrCode = require("qrcode-reader");
 const jimp = require("jimp");
+// const hh = require("../../QRS");
 const fs = require("fs");
-const path = require("path");
 const userModel = require("../../models/userModel");
 const meetingModel = require("../../models/meetingModel");
-
 const scan = async (request, response) => {
   if (request.file) {
     try {
-      const qrFilePath = path.join(__dirname, "../../../QRS", request.file.filename);
-      console.log("Uploaded file:", request.file.filename);
-      console.log("File path:", qrFilePath);
-      
-      if (!fs.existsSync(qrFilePath)) {
-        return response.json({ status: "Error", message: "File does not exist." });
-      }
-
-      const buffer = fs.readFileSync(qrFilePath);
+      //read qr code
+      const buffer = fs.readFileSync(
+        __dirname + ../../../QRS/${request.file.filename}
+      );
+      //read qr code as a buffer
       const image = await jimp.read(buffer);
       const qrcode = new QrCode();
-
       qrcode.callback = async (err, value) => {
         if (err) {
           console.error(err);
           return response.json({ message: "Invalid QR Code", error: err });
         } else {
-          const resultData = JSON.parse(value.result);
+          //convert data from string to json
+          const resultData = await JSON.parse(value.result);
+          console.log(resultData);
+          //find user
           const user = await userModel.findOne({ _id: request.id });
           if (!user) {
-            return response.json({ status: "Error", message: "User Not found" });
+            return response.json({
+              status: "Error",
+              message: "Oops!,User Not found",
+            });
           }
-
-          const meeting = await meetingModel.findOne({ meetingName: resultData[0].meetingName });
+          console.log(user);
+          //find meeting
+          const meeting = await meetingModel.findOne({
+            meetingName: resultData[0].meetingName,
+          });
           if (!meeting) {
-            return response.json({ status: "Error", message: "Meeting Not Found" });
+            return response.json({
+              status: "Error",
+              message: "oops!,Meeting Is Not Founds",
+            });
           }
+          console.log(meeting);
           if (user.meeting.includes(meeting._id)) {
-            return response.json({ status: "Error", message: "Attendance already recorded for this meeting" });
+            return response.json({
+              status: "Error",
+              message: "Oops!,You Already Record Attendance fro this meeting",
+            });
           }
-
           user.meeting.push(meeting._id);
           await user.save();
-          return response.json({ status: "Success", message: "Meeting Attendance Recorded Successfully" });
+          return response.json({
+            status: "Success",
+            message: "Congratularrions,Meeting Attendance Reorded Succefully",
+          });
         }
       };
-
       qrcode.decode(image.bitmap);
-      
-      if (fs.existsSync(qrFilePath)) {
-        fs.unlinkSync(qrFilePath);
-      }
+      fs.unlinkSync(__dirname + ../../../QRS/${request.file.filename});
     } catch (err) {
-      console.error("Error:", err.message);
       return response.json({ status: "Error", message: err.message });
     }
   } else {
-    return response.json({ status: "Error", message: "No File To Upload" });
+    return response.json({
+      status: "Error",
+      message: "Oops!,No File To Upload",
+    });
   }
 };
-
 module.exports = scan;
+ this code oois runnng willl in local host =but whne upload it on gihub and deploy it to render it  dosen't work
